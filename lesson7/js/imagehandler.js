2e5508dc8f7d202aedbb24e98d94f4cdafd41e04
rdeluna801 <rdeluna801@byui.edu>
2022-06-02 20:29:17 -0600

    stylesheets images lesson 7

--- lesson7/js/imagehandler.js
+++ lesson7/js/imagehandler.js
@@ -1,8 +1,8 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px, 0px, 50px, 0px"
+    threshold: 0,
+    rootMargin: "0px, 0px, -50px, 0px"
 };
 
 const loadImages = (image) => {
