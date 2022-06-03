af3942c88b6d1c80aea9bcddc306434f3af54431
rdeluna801 <rdeluna801@byui.edu>
2022-06-03 14:30:47 -0600

    lazyloading

--- lesson7/js/imagehandler.js
+++ lesson7/js/imagehandler.js
@@ -1,27 +1,29 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const images = document.querySelectorAll("src")
+
+function preloadImage(img) {
+    const src = img.getAttribute("src");
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+}
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px, 0px, -50px, 0px"
+    rootMargin: "0px 0px -100px 0px"
 };
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
-
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {});
-    }, imgOptions);
-
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    });
-} else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-}
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+        }
+        imgObserver.unobserve(entry.target);
+    })
+}, imgOptions);
+images.forEach(image => {
+    imgObserver.observe(image);
+});
