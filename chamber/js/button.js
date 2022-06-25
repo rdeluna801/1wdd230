53d913d23c1000edbb98550c9daa70d82ab7085a
rdeluna801 <rdeluna801@byui.edu>
2022-06-25 16:38:35 -0600

    changed nav

--- chamber/js/button.js
+++ chamber/js/button.js
@@ -1,7 +1,15 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-  }
-  
-  const x = document.getElementById("hamburgerBtn");
-  x.onclick = toggleMenu;
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
