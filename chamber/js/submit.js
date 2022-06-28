6dbd522e13adc5581d6169357882a8d5f29a1227
rdeluna801 <rdeluna801@byui.edu>
2022-06-28 02:06:56 -0600

    fixing form

--- chamber/js/submit.js
+++ chamber/js/submit.js
@@ -1,29 +1,22 @@
 const kp1 = document.querySelector("#keyphrase");
 const kp2 = document.querySelector("#keyphrase2");
 const message = document.querySelector("#message");
+const newPage = document.querySelector("#btnSubmit");
 
 kp2.addEventListener("focusout", checkSame);
+newPage.addEventListener("submit", page);
 
+function page()
+if (kp1.value == kp2.value && newPage) {
+
+}
 // This should be refactored.
 function checkSame() {
     if (kp1.value !== kp2.value) {
-        message.textContent = "Please enter a matching key phrase";
-        message.style.display = "block";
-        kp2.style.border = "black";
-        kp2.style.color = "#fff";
+        alert("Please enter a matching key phrase");
         kp2.focus();
         kp2.value = "";
     } else {
-        message.style.display = "none";
-        kp2.style.backgroundColor = "#fff";
-        kp2.style.color = "#000";
+        message.textContent = "Thank You";
     }
 }
-
-//function errorfont() {
-//if (Input.name("fname") = //"invalid") {
-//input.name("fname").//placeholder.display = "white";
-//  } else {
-//  input.name("fname").//placeholder.display = //"black";
-// }
-//}
