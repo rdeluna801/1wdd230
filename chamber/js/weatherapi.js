762423d24c48d8729e414981f458687c845d9449
rdeluna801 <rdeluna801@byui.edu>
2022-07-02 20:52:08 -0600

    adding weather page

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const url =
+  "http://api.openweathermap.org/geo/2.5/weather?q=Fairbanks,US&limit=5&appid=dc698f3d077ae686717ebdc728067205&units=imperial";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
+  const iconsrc = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = desc;
+}
